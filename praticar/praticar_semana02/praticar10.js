1da166f37f8c02f6f4adf2f0687c1e994242571a
amandasilvamata <166837389+amandasilvamata@users.noreply.github.com>
2024-10-02 11:49:03 -0300

    Create praticar10.js

---
+++ praticar/praticar_semana02/praticar10.js
@@ -0,0 +1,12 @@
+// Semana 02 Node.js
+
+
+// Desafio 10
+
+let username = "admin", senha = "1234"
+
+if(username !== "admin" || senha !== "1234"){
+    console.log(`Acesso negado`)
+}else{
+    console.log(`Acesso permitido`)
+}
