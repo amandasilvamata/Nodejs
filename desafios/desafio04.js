a1875b2aaa714f0ed5b87ce0b2b4554bfa79dc7f
amandasilvamata <166837389+amandasilvamata@users.noreply.github.com>
2024-10-04 23:19:43 -0300

    Create desafio04.js

---
+++ desafios/desafio04.js
@@ -0,0 +1,51 @@
+// Semana 04 Node.js
+
+// Hora do desafio 04
+
+class Livro{
+    constructor(titulo, autor, anoPublicacao, disponivel){
+        this.titulo = titulo
+        this.autor = autor
+        this.anoPublicacao = anoPublicacao
+        this.disponivel = disponivel
+    }
+    exibirInformacoes(){
+        console.log(`Título: ${this.titulo} \nAutor: ${this.autor} \nAno da publicação: ${this.anoPublicacao} \nDisponível: ${this.disponivel}`)
+    }
+    emprestar(){
+        if(this.disponivel == true){
+            console.log(`****** O livro ******\n`) 
+            this.exibirInformacoes()
+            console.log(`\n****** está disponível ******\n`)
+            
+            this.disponivel = false
+        }else{
+            console.log(`****** O livro ******\n`) 
+            this.exibirInformacoes()
+            console.log(`\n****** NÃO está disponível ******\n`)
+        }
+    }
+    devolver(){
+        this.disponivel = true
+        console.log(`****** O livro ******\n`) 
+            this.exibirInformacoes()
+            console.log(`\n****** foi devolvido ******\n`)
+    }
+}
+
+let livro1 = new Livro("Sapiens: Uma Breve História da Humanidade", "Yuval Harari", 2011, true)
+
+let livro2 = new Livro("Breves respostas para grandes questões", "Stephen Hawking",2018, false )
+
+
+livro1.exibirInformacoes()
+console.log(`\n++++++++++++++++++++++++++++++++++\n`)
+livro2.exibirInformacoes()
+console.log(`\n++++++++++++++++++++++++++++++++++\n`)
+livro1.emprestar()
+console.log(`\n++++++++++++++++++++++++++++++++++\n`)
+livro2.emprestar()
+console.log(`\n++++++++++++++++++++++++++++++++++\n`)
+livro2.devolver()
+livro2.emprestar()
+console.log(`\n++++++++++++++++++++++++++++++++++\n`)
