1f8b0280fbd9176b78ce48663d5a54d66b8caab6
amandasilvamata <166837389+amandasilvamata@users.noreply.github.com>
2024-10-19 11:18:14 -0300

    Create app.js

---
+++ aulas/Aula 05/Arrow/app.js
@@ -0,0 +1,13 @@
+const somar = (a,b) => a + b
+
+console.log(somar(4,3))
+
+const calcularIdade = (anoNasc, anoAtual) => {
+    return anoAtual - anoNasc
+}
+
+console.log(calcularIdade(2003,2024))
+console.log(calcularIdade(1990,2024))
+
+
+
