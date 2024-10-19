73ac57cc94d8c2975f647984a60a3e73bb54392e
amandasilvamata <166837389+amandasilvamata@users.noreply.github.com>
2024-10-19 11:20:46 -0300

    Create app.js

---
+++ aulas/Aula 05/Modulos/app.js
@@ -0,0 +1,15 @@
+// Código principal
+const funcaoSoma = require('./somar')
+
+const funcaoSubtracao = require('./sub')
+
+const funcaoMultiplicacao = require('./mult')
+
+const funcaoDivisao = require('./div')
+
+
+
+console.log(`O resultado da soma é: ${funcaoSoma(4,3)}`)
+console.log(`O resultado da subtração é: ${funcaoSubtracao(4,3)}`)
+console.log(`O resultado da  multiplicação é: ${funcaoMultiplicacao(4,3)}`)
+console.log(`O resultado da divisão é: ${funcaoDivisao(4,3)}`)
